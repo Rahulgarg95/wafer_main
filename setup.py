2a83b649f53c84e45ad9598178dad90a371d5d37
Rahul Garg <rahulgarg95@gmail.com>
2021-03-28 18:24:20 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of wafer fault detection project using MLOPS',
+    author='rahulgarg95',
+    license='MIT',
+)
